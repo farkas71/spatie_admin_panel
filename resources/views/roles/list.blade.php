28dc21bf8965f987d51f16988ea892d91f235232
farkas71 <farkasmisi1222@gmail.com>
2023-11-07 14:53:26 +0100

    szerepkör lista jogosultság beállítás

--- resources/views/roles/list.blade.php
+++ resources/views/roles/list.blade.php
@@ -11,70 +11,45 @@
                     <th>id</th>
                     <th style="width: 150px;">Szerepkör neve</th>
                     <th style="width: 150px;">Guard név </th>
-                    <th></th>
-                    <th>Engedélyek</th>
+                    @can('delete users')
+                        <th></th>
+                        <th>Engedélyek</th>
+                    @else
+                        <th>Engedélyek száma</th>
+                    @endcan
                     <th>Műveletek</th>
                 </tr>
             </thead>
             <tbody>
-                {{-- @foreach ($usersWithRoles as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>
-                            @foreach ($user->roles as $role)
-                                <span class="badge bg-danger me-1">{{ $role->name }}</span>
-                            @endforeach
-                        </td>
-                        <td>
-                            @foreach ($user->roles as $role)
-                                @foreach ($role->permissions as $permission)
-                                    <span class="badge bg-danger me-1">{{ $permission->name }}</span>
-                                @endforeach
-                            @endforeach
-                        </td>
-                        <td>
-                            <div class="d-flex justify-content-center">
-                                <a class="btn btn-sm btn-outline-success" href="#">szerkeszt</a>
-                                <form action="#" method="GET">
-                                    @csrf
-                                    <button class="btn btn-sm btn-outline-danger ms-1" type="submit">Törlés</button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach --}}
-
                 @foreach ($rolesWithPermissions as $role)
                     <tr>
                         <td class="text-center">{{ $role->id }}</td>
                         <td class="ps-4"><b>{{ $role->name }}</b></td>
                         <td class="text-center">{{ $role->guard_name }}</td>
-                        <td>
+                        <td class="text-center">
                             <span class="badge bg-danger">{{ $role->permissions->count() }}</span>
                         </td>
-                        <td>
-                            @foreach ($role->permissions as $permission)
-                                <span class="badge bg-danger me-1">{{ $permission->name }}</span>
-                            @endforeach
-                        </td>
-
+                        @can('delete users')
+                            <td>
+                                @foreach ($role->permissions as $permission)
+                                    <span class="badge bg-danger me-1">{{ $permission->name }}</span>
+                                @endforeach
+                            </td>
+                        @endcan
                         <td>
                             <div class="d-flex justify-content-center">
                                 <a class="btn btn-sm btn-outline-success" href="#">szerkeszt</a>
-                                <form action="#" method="GET">
-                                    @csrf
-                                    <button class="btn btn-sm btn-outline-danger ms-1" type="submit">Törlés</button>
-                                </form>
+                                @can('delete users')
+                                    <form action="#" method="GET">
+                                        @csrf
+                                        <button class="btn btn-sm btn-outline-danger ms-1" type="submit">Törlés</button>
+                                    </form>
+                                @endcan
                             </div>
                         </td>
                     </tr>
                 @endforeach
-
             </tbody>
         </table>
-
-
     </div>
 </x-app-layout>
